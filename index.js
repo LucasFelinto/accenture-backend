db2ed4ab902e5aa0bf17d34147bca130e4289e3f
LucasFelinto <elucasfelinto@gmail.com>
2020-10-04 17:23:18 -0300

    index

--- index.js
+++ index.js
@@ -3,24 +3,45 @@ const app = express();
 
 app.use(express.json());
 
-const letters = [
-  ['a', 'b', 'c'],
-  ['d', 'e', 'f'],
-  ['g', 'h', 'i'],
-  ['j', 'k', 'l'],
-  ['m', 'n', 'o'],
-  ['p', 'q', 'r', 's'],
-  ['s', 'u', 'v'],
-  ['w', 'x', 'y', 'z'],
-];
+const letters = {
+  2: ['a', 'b', 'c'],
+  3: ['d', 'e', 'f'],
+  4: ['g', 'h', 'i'],
+  5: ['j', 'k', 'l'],
+  6: ['m', 'n', 'o'],
+  7: ['p', 'q', 'r', 's'],
+  8: ['s', 'u', 'v'],
+  9: ['w', 'x', 'y', 'z'],
+}
+
+function translateMessage(number, total) {
+
+  if (letters[number]) {
+    const tamanho = letters[number].length;
+    const index = total % tamanho - 1;
+    console.log(index);
+    return letters[number][index];
+
+  } else {
+    return;
+  }
+
+}
 
 app.post('/', (req, res) => {
-  console.log(req.body)
+  const {
+    number,
+    total
+  } = req.body;
+  console.log(translateMessage(number, total));
+  res.json({
+    "msg": "oi"
+  })
 });
 
 const PORT = 8002;
 
-app.listen(PORT,() => {
+app.listen(PORT, () => {
   console.log('Servidor iniciado na porta 8001');
   console.log('CTRL + C para derrubar o servidor');
 })
