8b6d1a542a5c9e31915ea0f4ce663fdf63139bbe
LucasFelinto <elucasfelinto@gmail.com>
2020-10-04 16:28:49 -0300

    Array de letras

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello Worl');
+const letters = [
+  ['a', 'b', 'c'],
+  ['d', 'e', 'f'],
+  ['g', 'h', 'i'],
+  ['j', 'k', 'l'],
+  ['m', 'n', 'o'],
+  ['p', 'q', 'r', 's'],
+  ['s', 'u', 'v'],
+  ['w', 'x', 'y', 'z'],
+];
+
+app.post('/', (req, res) => {
+  console.log(req.body)
 });
 
 app.listen(8002);
